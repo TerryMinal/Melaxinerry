adaaee0a74c1b8789c124fdd599c270eacc9c708
Terry Guan <terryguan513@gmail.com>
2017-01-11 11:02:45 -0500

    edited Player.java

--- UNO/Player.java
+++ UNO/Player.java
@@ -1,11 +1,17 @@
 public class Player{
-    private String name;
-    private int score;
-    private int pin; //4-digit
-
+    protected String name;
+    protected int score;
+    protected int pin; //4-digit
+    protected ArrayList 
+    
     public Player(){
 	score = 0;
 	pin = 0000;
     }//end constructor
     
+    public void playCard(int index) {
+	
+    }
+
+
 }//end class
