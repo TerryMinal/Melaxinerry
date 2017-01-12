d4f05f202f8915128bdba08d7e748eea89151280
melaniechow <mchow@stuy.edu>
2017-01-12 05:01:22 -0500

    update

--- UNO/WOO.java
+++ UNO/WOO.java
@@ -4,26 +4,41 @@ public class WOO{
     int numRealPlayers;
     ArrayList<Player> allPlayers; 
     ArrayList<Deck> discarded;
-    
+ 
+    //instantiate players and put them in arraylist  
     public WOO() {
 	allPlayers = new ArrayList<Player>(); 
-	discarded = new ArrayList<Deck>(); 
+	discarded = new ArrayList<Deck>();
     }
 
-    public void game() {
-    
-    }
-    public void distribute(ArrayList<Deck> deck) {
+   public void distribute(ArrayList<Deck> deck) {
 	for (int i = 0; i < allPlayers.size(); i++) {
 	    for (int n = 0; n < 7; n++) {
 		allPlayers.get(i).add(); 
 	    }
 	}
+   }
+
+    //returns index of player that goes first;
+    public Player rolldice(){
+        int first=Math.random()*(allPlayers.size()) //[0,size-1]
+	return first;
+	//rearranges allPlayer order list (eg. 01234 -->2-->23401)
+	for (int i=0; i<allPlayers.size()-first;i++){
+	    allPlayers.add(i,allPlayers.remove(first+i));
+	}
     }
+
+    /*	
+    public void game() {
+	
     
+    }
+    */
     public static void main(String[] args){
         //prints an ArrayList of the whole deck
 	System.out.println(Deck.createDeck());
+	
 	
     }//end main
     
