28ac2082a3c25684fa428a0406a722772c226ab5
TerryMinal <tguan@stuy.edu>
2017-01-11 21:01:46 -0500

    added sort method skeletons into Player.java. Added primitive drawCard() method

--- UNO/Player.java
+++ UNO/Player.java
@@ -1,21 +1,42 @@
 public class Player{
     protected String name;
     protected int score;
-    protected int pin; //4-digit
-    protected ArrayList <Card> currentCards;
+    //protected int pin; //4-digit
+    protected ArrayList <Deck> currentCards;
     
-    public Player(){
+    public Player(String name){
+	this.name = name;
 	score = 0;
-	pin = 0000;
+	//pin = 0000;
+	currentCards = new ArrayList<Deck>();
     }//end constructor
     
     public void playCard(int index) {
 	
     }
 
-    public void draw(){
+    public int setScore(int score) {
+	int oldScore = this.score;
+	this.score = score;
+	return oldScore;
+    }
+    
+    public void draw(ArrayList<Deck> deck){
+	currentCards.add(deck.get(0));
     }
 
+    void sortCards(int whichSort) {
+    }
+
+    void sortCardsColor() {
+    }
+
+    void sortCardsNum() {
+    }
+
+    void sortCardsWild() {
+    }
+    
     public void callUNO(){
     }
     
