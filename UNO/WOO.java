3a68d24454ac6ea0b5315d724cad0e7bd08f20a8
TerryMinal <tguan@stuy.edu>
2017-01-11 21:07:57 -0500

    added distribute method to Woo.java

--- UNO/WOO.java
+++ UNO/WOO.java
@@ -1,4 +1,15 @@
 public class WOO{
+
+    int numAi;
+    int numRealPlayers;
+    ArrayList<Player> allPlayers; 
+    public void distribute(ArrayList<Deck> deck) {
+	for (int i = 0; i < allPlayers.size(); i++) {
+	    for (int n = 0; n < 7; n++) {
+		allPlayers.get(i).add(); 
+	    }
+	}
+    }
     
     public static void main(String[] args){
         //prints an ArrayList of the whole deck
