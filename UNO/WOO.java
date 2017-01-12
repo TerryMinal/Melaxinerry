2cec54c7056407d8e11760c4f4f88b271cd24ca9
TerryMinal <tguan@stuy.edu>
2017-01-12 10:40:07 -0500

    updatE

--- UNO/WOO.java
+++ UNO/WOO.java
@@ -3,6 +3,16 @@ public class WOO{
     int numAi;
     int numRealPlayers;
     ArrayList<Player> allPlayers; 
+    ArrayList<Deck> discarded;
+    
+    public WOO() {
+	allPlayers = new ArrayList<Player>(); 
+	discarded = new ArrayList<Deck>(); 
+    }
+
+    public void game() {
+    
+    }
     public void distribute(ArrayList<Deck> deck) {
 	for (int i = 0; i < allPlayers.size(); i++) {
 	    for (int n = 0; n < 7; n++) {
