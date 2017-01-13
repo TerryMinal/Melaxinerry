3bcb6c5468c9f905fc29e829b447412714239f8f
melaniechow <mchow@stuy.edu>
2017-01-12 22:14:54 -0500

    start screen menu

--- UNO/Woo.java
+++ UNO/Woo.java
@@ -1,5 +1,6 @@
 
 import java.util.*;
+import cs1.Keyboard;
 
 public class Woo {
 
@@ -9,7 +10,7 @@ public class Woo {
     ArrayList<Card> discardPile;
     Deck drawPile;
     //instantiate players and put them in arraylist  
-    public WOO() {
+    public Woo() {
 	numAi = 0;
 	numRealPlayers = 0;	
 	allPlayers = new ArrayList<Player>();
@@ -45,10 +46,58 @@ public class Woo {
 
     //start screen etc goes here
     public void beginGame() {
-	numAi = 0;
-	numRealPlayers = 2;
-	allPlayers.add(new Player("Terry"));
-	allPlayers.add(new Player("Guan"));
+	int typegame=0;
+	//----STRINGS-----------
+	String rules="How to Play:\n";
+	rules+="A game of UNO consists of 2-5 players. Each player starts with 7 cards \n";
+	rules+="A dice is rolled to see who goes first.\n";
+	rules+="On a players turn, he/she must do ONE of the following: Draw or Play or Call UNO.\n";
+	rules+="-play a card matching the discard in either color, number, or symbol\n";
+	rules+="-play a Wild card, or a playable Wild Draw Four card\n";
+	rules+="-draw a card from the deck.\n";
+	rules+="-you must call UNO when you have one card left. If you didn¡¯t call UNO before your turn is over, you will draw 2 cards. If you call UNO when you have more than one card, you will draw 4 cards.\n";
+	rules+="How to Win:\n";
+	rules+="The first player to get rid of his/her last card wins the round and scores points for the cards held by the other players.Number cards count their face value, all action cards count 20, and Wild and Wild Draw Four cards count 50. If a Draw Two or Wild Draw Four card is played to go out, the next player in sequence must draw the appropriate number of cards before the score is tallied. \n";
+	rules+="First player to reach 500 points wins. \n";
+	rules+="==============================================\n";
+
+	String start="Lets start! \n"; 
+	start+="Enter 1 for Solo! \n";
+	start+="Enter 2 for multiplayer \n";
+	start+="Enter 3 for rules \n";
+
+	//-----------------------
+
+	System.out.println("Welcome to UNO! \n");
+	while (typegame< 1 || typegame > 3){
+	    System.out.print(start);
+	    typegame=Keyboard.readInt();
+	    if (typegame==3){
+		System.out.print(rules);
+		typegame=0;
+	    }
+	}
+	if (typegame==1){
+	    /* this part prob goes somewhere else
+	    System.out.print("How would thy like to be called: ");
+	    name=Keyboard.readString();
+	    System.out.print("Enter a PIN to access your account");
+	    pass=Keyboard.readString();
+	    */
+	    numRealPlayers=1;
+	    System.out.print("How many computers? (Max 4): "); //does not take into accunt when number is greater than 4
+	    numAi=Keyboard.readInt();
+	    numRealPlayers=1;
+	}
+	if (typegame==2){ 
+	    System.out.print("How many players?:"); //does not take into account if # of players >5
+	    numRealPlayers=Keyboard.readInt();
+	    System.out.print("How many computers?:");
+	    numAi=Keyboard.readInt();
+	}
+
+	allPlayers.add(new Player("hiya1"));
+	allPlayers.add(new Player("hiya"));
 	distribute();
     }
 
