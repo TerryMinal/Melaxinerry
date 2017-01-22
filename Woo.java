922d6ba6e9cacd93a40711a502c20fa5a59af49f
TerryMinal <tguan@stuy.edu>
2017-01-22 13:32:45 -0500

    Revert "fixed clearScreen() so user cannot scroll back"
    This reverts commit 4424a670424499192893fa47bf7c133941b265f0.

--- Woo.java
+++ Woo.java
@@ -71,11 +71,9 @@ public class Woo {
 	return retStr;
     }
     
-    public static void clearScreen() {
-	String cls="printf \"\033c\"";
-       	System.out.println(cls);
-	//System.out.print("\033[H\033[2J");  
-	//System.out.flush();  	
+    public static void clearScreen() {  
+	System.out.print("\033[H\033[2J");  
+	System.out.flush();  
     }
     
     public void beginGame() {
